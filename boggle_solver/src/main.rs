c9bafebf3106836ae08c1d1edb3fe5b7951182d6
Zachary Voltz <zachary@voltz.us>
2023-12-04 21:55:53 -0600

    Sort Words, Fix Bug

--- boggle_solver/src/main.rs
+++ boggle_solver/src/main.rs
@@ -1,4 +1,3 @@
-use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Instant;
 use std::collections::HashSet;
@@ -8,7 +7,7 @@ use lazy_static::lazy_static;
 
 const ALPHABET_SIZE: usize = 26;
 
-#[derive(Default)]
+#[derive(Default, Clone)]
 struct TrieNode {
     children: [Option<Box<TrieNode>>; ALPHABET_SIZE],
     is_end_of_word: bool,
@@ -79,10 +78,9 @@ const DIRECTIONS: [(i32, i32); 8] = [
     (-1, -1), (0, -1), (1, -1)
 ];
 
-fn add_word(words: &Arc<Mutex<HashSet<String>>>, word: String) {
-    if word.len() > 2 && search(&*ROOT, &word) {
-        let mut words_set = words.lock().expect("Mutex lock failed");
-        words_set.insert(word);
+fn add_word(words: &mut Vec<String>, word: &str, root: &TrieNode) {
+    if word.len() > 2 && search(root, word) {
+        words.push(word.to_string());
     }
 }
 
@@ -95,51 +93,56 @@ fn valid_recurse(new_location: (usize, usize), visited_so_far: &Vec<(usize, usiz
     !visited_so_far.contains(&new_location)
 }
 
-fn solve_recursive(board: Vec<Vec<String>>, visited_so_far: &mut Vec<(usize, usize)>, location: (usize, usize), word_so_far: String, words: &Arc<Mutex<HashSet<String>>>) {
-    visited_so_far.push(location);
-    add_word(words, word_so_far.clone());
-    if is_leaf(&*ROOT, &word_so_far) {
-        visited_so_far.pop();
-        return;
+fn solve_recursive(board: Vec<Vec<String>>, location: (usize, usize), word_so_far: String, root: &TrieNode) -> Vec<String> {
+    let mut words_found = Vec::new();
+    let visited_so_far = vec![location];
+    add_word(&mut words_found, &word_so_far, root);
+
+    if is_leaf(root, &word_so_far) {
+        return words_found;
     }
-    // go in all directions
+
     for &direction in &DIRECTIONS {
         let new_location = ((location.0 as i32 + direction.0) as usize, (location.1 as i32 + direction.1) as usize);
         if valid_recurse(new_location, &visited_so_far, &board) {
-            visited_so_far.push(new_location);
             let new_word_so_far = word_so_far.clone() + &board[new_location.0][new_location.1];
-            solve_recursive(board.clone(), visited_so_far, new_location, new_word_so_far, words);
-            visited_so_far.pop();
+            let mut new_visited = visited_so_far.clone();
+            new_visited.push(new_location);
+            let mut words_from_direction = solve_recursive(board.clone(), new_location, new_word_so_far, root);
+            words_found.append(&mut words_from_direction);
         }
     }
+
+    words_found
 }
 
-fn solve_thread(letters: Vec<Vec<String>>, row: usize, col: usize, words: &Arc<Mutex<HashSet<String>>>) {
-    let mut visited_so_far = Vec::new(); // Each thread has its own visited_so_far vector
-    let location = (row, col); // Each thread has its own location array
-    solve_recursive(letters.clone(), &mut visited_so_far, location, letters[row][col].clone(), words);
+fn solve_thread(letters: Vec<Vec<String>>, row: usize, col: usize, root: &TrieNode) -> Vec<String> {
+    let location = (row, col);
+    solve_recursive(letters.clone(), location, letters[row][col].clone(), root)
 }
 
-fn solve(letters: Vec<Vec<String>>) -> HashSet<String> {
+fn solve(letters: Vec<Vec<String>>, root: &TrieNode) -> HashSet<String> {
     let start = Instant::now();
-    let words = Arc::new(Mutex::new(HashSet::new()));
     let mut threads = Vec::new();
+    let mut words_found = Vec::new();
+
     for row in 0..letters.len() {
         for col in 0..letters[0].len() {
-            let words_clone = words.clone();
             let letters_clone = letters.clone();
-            threads.push(thread::spawn(move || solve_thread(letters_clone, row, col, &words_clone)));
+            let root_clone = root.clone();
+            threads.push(thread::spawn(move || solve_thread(letters_clone, row, col, &root_clone)));
         }
     }
 
-    // Wait for all threads to finish
+    // Wait for all threads to finish and collect results
     for thread in threads {
-        thread.join().expect("Thread join failed");
+        let mut thread_result = thread.join().expect("Thread join failed");
+        words_found.append(&mut thread_result);
     }
 
     let elapsed = start.elapsed();
     println!("Solving took: {} seconds", elapsed.as_secs());
-    Arc::try_unwrap(words).expect("Failed to unwrap Arc").into_inner().expect("Failed to unwrap Mutex")
+    words_found.into_iter().collect()
 }
 
 fn main() {
@@ -153,6 +156,15 @@ fn main() {
         let curr_line_vec: Vec<String> = curr_line.to_lowercase().split_whitespace().map(String::from).collect();
         board.push(curr_line_vec);
     }
-    let words = solve(board);
+    let root = &*ROOT;
+    let mut words: Vec<String> = solve(board, root).into_iter().collect();
+    words.sort_by(|a, b| {
+        let length_cmp = b.len().cmp(&a.len());
+        if length_cmp == std::cmp::Ordering::Equal {
+            a.cmp(b)
+        } else {
+            length_cmp
+        }
+    });
     println!("Found {} Words: {:?}", words.len(), words);
 }
